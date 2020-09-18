522f5d5135da179e712e9cf3a5ed5b99f78cfa02
nkeirn <70721761+nkeirn@users.noreply.github.com>
2020-09-17 20:18:26 -0400

    Add files via upload

---
+++ Tech Proj 2b/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Tech_Proj_2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What grade do you expect to get from ISM 4300. Enter a grade from 1-100!");
+
+            try
+            {
+                // This variable will gather the user input
+                string input = Console.ReadLine();
+                decimal grade = decimal.Parse(input);
+                int grades = 0;
+                if (grades > 90)
+                {
+                    Console.WriteLine("Your grade is an A");
+                }
+                else if ((grades > 80) && (grades < 90))
+                {
+                    Console.WriteLine("Your grade is a B");
+                }
+                else if ((grades > 70) && (grades < 80))
+                {
+                    Console.WriteLine("Your grade is a C");
+                }
+                else if ((grades > 60) && (grades < 70))
+                {
+                    Console.WriteLine("Your grade is a D");
+                }
+                else if (grades <60)
+                {
+                    Console.WriteLine("Your grade is an F");
+                }
+                else
+                {
+                    Console.WriteLine("This is not a valid answer");
+                
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a grade between 1 and 100");
+            }
+        }
+    }
+}
